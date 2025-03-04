5891f7437db09efb3756b108fe34ea6f35992197
Texas R3X <texasr3xphone@gmail.com>
2025-03-04 08:01:13 -0700

    Added Postgres; started student-middleware.js; added test/

---
+++ server/middleware/student-middleware.js
@@ -0,0 +1,8 @@
+export function signUp(req, res) {
+    
+}
+
+
+export function getStudentClasses(req, res) {
+
+}
--- server/server.js
+++ server/server.js
@@ -11,12 +11,35 @@ app.use(express.urlencoded({ extended: true }));
 setUpMorgan(app);
 
 
+import pg from "pg";
+const { Pool } = pg;
+const connectionString = process.env.DB_URI || 'postgres://postgres:postgres@localhost:5432/capstone-test';
+logger.info(`connectionString: ${connectionString}`);
+
+const pool = new Pool({ connectionString });
+
+
+
 app.get("/", (_req, res) => {
     res.send("Hello World!");
 });
 
 app.get("/test", (_req, res) => {
     res.send("Test Endpoint");
+});
+
+app.post("/test-query", async (req, res) => {
+    try {
+        const data = await pool.query({
+            text: "SELECT * FROM courses;",
+            values: []
+        });
+
+        res.json(data.rows)
+    } catch (err) {
+        logger.info(`err: ${err}`);
+        res.status(500).send("Error");
+    }
 });
 
 
