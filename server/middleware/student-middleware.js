d157017d24c8c9563aff1d5405b925afd2f9c5e3
Texas R3X <texasr3xphone@gmail.com>
2025-03-07 07:47:45 -0700

    POST /api/user developed

--- server/middleware/student-middleware.js
+++ server/middleware/student-middleware.js
@@ -4,53 +4,49 @@ import pgPool from "../services/postgres.js";
 import logger from "../logging/logger.js";
 import { genToken } from "../services/jwt.js";
 
-export async function getRegisterMW(req, res) {
+export async function postUserMW(req, res) {
     try {
-        // Store address data: (Incomplete)
-        const addressData = { ...req.body.address_data };
-
-        // Store general data:
-        const generalData = {
-            ...req.body.general_data,
-            password_hash: bcrypt.hashSync(req.body.general_data.password, 10),
+        const userData = {
+            ...req.body,
+            password_hash: bcrypt.hashSync(req.body.password, 10),
             user_id: genUserID(),
-            address_id: null
         };
-        delete generalData.password;
-
 
         const { rowCount } = await pgPool.query({
             text: `
                 INSERT INTO
-	                users (user_id, first_name, last_name, username, password_hash, email, phone_number, address_id, is_admin)
+	                users (user_id, first_name, last_name, password_hash, email, phone_number, is_admin, address_id)
                 VALUES
-	                ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+	                ($1, $2, $3, $4, $5, $6, $7, find_or_create_address($8, $9, $10, $11));
             `,
             values: [
-                generalData.user_id,
-                generalData.first_name,
-                generalData.last_name,
-                generalData.username,
-                generalData.password_hash,
-                generalData.email,
-                generalData.phone_number,
-                generalData.address_id,
-                generalData.is_admin
+                userData.user_id, // $1
+                userData.first_name, // $2
+                userData.last_name, // $3
+                userData.password_hash, // $4
+                userData.email, // $5
+                userData.phone_number, // $6
+                userData.is_admin, // $7
+
+                userData.address_data.street, // $8
+                userData.address_data.city, // $9
+                userData.address_data.state_or_region, // $10
+                userData.address_data.country // $11
             ]
         });
 
-        if (!!rowCount) {
-            const token = genToken(generalData.user_id, generalData.username);
-
-            res.json({ token });
+        if (rowCount) {
+            const token = genToken(userData.user_id, userData.username);
+            res.json({
+                token,
+                userID: userData.user_id
+            });
         }
         else res.status(500).json({ errorMessage: "Postgres Error" });
+
     } catch (err) {
         logger.error(err.message);
-        if (err.message == `duplicate key value violates unique constraint "username"`) {
-            res.status(409).json({ errorMessage: "Username already in use" });
-        }
-        // else if (err.message == `duplicate key value violates unique constraint "users_pkey"`) {
+        // if (err.message == `duplicate key value violates unique constraint "users_pkey"`) {
 
         // }
 
@@ -86,6 +82,6 @@ export async function getUserMW(req, res) {
         else res.status(404).json({ errorMessage: "User does not exist" });
     } catch (err) {
         logger.error(err.message);
-        res.status(500).json({ errorMessage: "Internal Server Error" });
+        res.status(500).json({ errorMessage: err.message });
     }
 }
