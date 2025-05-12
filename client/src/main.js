60bcaefaab2e2bf5851d5e82347584b4cbaa28ae
Texas R3X <texasr3xphone@gmail.com>
2025-05-12 08:55:15 -0600

    Removed element-plus dependency

--- client/src/main.js
+++ client/src/main.js
@@ -3,13 +3,10 @@ import './style.css';
 import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
-import { ElSwitch } from 'element-plus';
-import 'element-plus/es/components/switch/style/css';
 
 const app = createApp(App);
 
 app.use(router);
 app.use(createPinia());
-app.component('ElSwitch', ElSwitch);
 
 app.mount('#app');
