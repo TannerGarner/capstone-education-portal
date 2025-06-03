899038228a52d725125ca29f4ea8bdcf99a4eec2
Tanner Garner <garnert.w.14@gmail.com>
2025-06-03 12:04:47 -0600

    Add dropdown menu to TopBar for the logout button

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -7,7 +7,7 @@
     const router = useRouter();
     const enrollmentStore = useEnrollmentStore();
 
-    const props = defineProps(['changeDisplay', 'components']);
+    const dropdownOpen = ref(false);
 
     const isAdmin = userStore.user.is_admin;
 
@@ -34,16 +34,14 @@
             <img src="/logo.png" alt="logo" class="logo">
             <h2>Vision<br>Academy</h2>
         </div>
-        <div class="user">
-            <img
-                src="../assets/vue.svg"
-                alt="user-picture"
-                class="userPic"
-                @dblclick="showUserStateForDebugging()"
-            />
-            <h3>{{ userStore.user.first_name }} {{ userStore.user.last_name }}</h3>
-            <button class="logout" @click="logout">Logout</button>
-        </div>
+        <img @click="dropdownOpen = !dropdownOpen" @dblclick="showUserStateForDebugging()"src="/logo.png" alt="profile-picture" class="profilePicture" />
+        <transition name="slide">
+            <div v-if="dropdownOpen" class="user">
+                <h3>{{ userStore.user.first_name }} {{ userStore.user.last_name }}</h3>
+                <button class="logout" @click="logout">Logout</button>
+            </div>
+        </transition>
+        
     </div>
 </template>
 
@@ -65,10 +63,27 @@
         gap: 10px;
     }
 
+    .profilePicture {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        cursor: pointer;
+        transition: transform 0.3s;
+    }
+
     .user {
         display: flex;
+        flex-direction: column;
+        position: absolute;
+        top: 100px;
+        right: 45px;
         align-items: center;
+        background-color: #F5F1ED;
+        border: #489FB5 2px solid;
+        border-radius: 15px;
         gap: 20px;
+        padding: 30px;
+        box-shadow: 0px 0px 10px rgba(21, 49, 49, 20%);
     }
 
     .logout{
