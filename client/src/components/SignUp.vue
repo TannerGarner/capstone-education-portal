0c963c1baa0ca98067fe82276f5e06111bd344f0
tannergarner <garnert.w.14@gmail.com>
2025-06-03 22:08:22 -0600

    Fix style error on Sign Up page

--- client/src/components/SignUp.vue
+++ client/src/components/SignUp.vue
@@ -41,17 +41,19 @@
                 <h1>Student Registration</h1>
             </div>
             <div class="inputs">
+                <div class="profile-pic-input input">
                     <input type="file" placeholder="Profile Pic">
-                    <input v-model="newUser.street" required type="text" name="street" placeholder="Street Address">
-                    <input v-model="newUser.first_name" required type="text" name="firstName" placeholder="First Name">
-                    <input v-model="newUser.city" required type="text" name="city" placeholder="City">
-                    <input v-model="newUser.last_name" required type="text" name="lastName" placeholder="Last Name">
-                    <input v-model="newUser.state" required type="text" name="stateRegion" placeholder="State, Region">
-                    <input v-model="newUser.email" required type="email" name="email" placeholder="Email">
-                    <input v-model="newUser.country" required type="text" name="country" placeholder="Country">
-                    <input v-model="newUser.password" required type="password" name="password" placeholder="Password">
-                    <input v-model="newUser.phone_number" required type="tel" name="phone" placeholder="Phone Number">
                 </div>
+                <input class="input" v-model="newUser.street" required type="text" name="street" placeholder="Street Address">
+                <input class="input" v-model="newUser.first_name" required type="text" name="firstName" placeholder="First Name">
+                <input class="input" v-model="newUser.city" required type="text" name="city" placeholder="City">
+                <input class="input" v-model="newUser.last_name" required type="text" name="lastName" placeholder="Last Name">
+                <input class="input" v-model="newUser.state" required type="text" name="stateRegion" placeholder="State, Region">
+                <input class="input" v-model="newUser.email" required type="email" name="email" placeholder="Email">
+                <input class="input" v-model="newUser.country" required type="text" name="country" placeholder="Country">
+                <input class="input" v-model="newUser.password" required type="password" name="password" placeholder="Password">
+                <input class="input" v-model="newUser.phone_number" required type="tel" name="phone" placeholder="Phone Number">
+            </div>
             <button type="submit" class="submit">Sign Up</button>
             <p>Already have an Account? <span @click="emit('toggle')">Login</span></p>
         </form>
@@ -110,7 +112,22 @@
         gap: 20px;    
     }
 
-    input {
+    input[type="file"] {
+        width: 100%;
+        max-width: 100%;
+        font-size: 18px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+    }
+    
+    .profile-pic-input {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .input {
         padding: 15px;
         padding-left: 0px;
         border: none;
@@ -122,12 +139,12 @@
         max-width: 100%;
     }
 
-    input::placeholder{
+    .input::placeholder{
         color:#153131;
         opacity: 50%;
     }
 
-    input:focus {
+    .input:focus {
         border-color: #FE5E41;        /* Custom border on focus */
         box-shadow: 0 0 5px #FE5E41;  /* Optional glow or highlight */
         outline: none;                /* Remove default outline */
@@ -178,7 +195,7 @@
             overflow-y: scroll;
         }
         
-        input{
+        .input{
             font-size: 18px;
         }
     }
